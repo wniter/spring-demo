7e13e9e405726699e1ce835e3769fdefed1c453e
Boolean <lyg2000@aliyun.com>
2022-03-20 05:32:34 +0800

    add springdata init

---
+++ springboot-springdata/src/main/java/com/example/springboot/springdata/SpringbootSpringdataApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringdataApplication.class, args);
+    }
+
+}
---
+++ springboot-springdata/src/test/java/com/example/springboot/springdata/SpringbootSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
