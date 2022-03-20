1cbade381424068968be5f82adbac3db6379ad76
Boolean <lyg2000@aliyun.com>
2022-03-21 01:35:17 +0800

    add

--- springboot-mybatisplus-iservice/src/main/java/com/example/springboot/mybatisplus/config/MybatisPlusPageConfig.java
+++ springboot-mybatisplus-iservice/src/main/java/com/example/springboot/mybatisplus/config/MybatisPlusPageConfig.java
@@ -1,4 +1,31 @@
 package com.example.springboot.mybatisplus.config;
 
+import com.github.pagehelper.PageInterceptor;
+import org.springframework.context.annotation.Bean;
+
 public class MybatisPlusPageConfig {
+    /**
+     * pagehelper的分页插件
+     */
+    @Bean
+    public PageInterceptor pageInterceptor() {
+        return new PageInterceptor();
+    }
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
+     *
+     * mp 与 pagehelper 存在依赖 jsqlparser 冲突，不建议混用
+     */
+//    @Bean
+//    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+//        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+//        return interceptor;
+//    }
+
+//    @Bean
+//    public ConfigurationCustomizer configurationCustomizer() {
+//        return configuration -> configuration.setUseDeprecatedExecutor(false);
+//    }
 }
