c014021aa3297df03eab6fa994a8db85f763b59a
Boolean <lyg2000@aliyun.com>
2022-03-22 03:43:30 +0800

    add

---
+++ springboot-ehcache/src/test/java/com/example/springboot/ehcache/SpringbootEhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.ehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEhcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
