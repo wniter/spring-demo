f0fa897e829cc47ed776428f20c8f4738519374c
Boolean <lyg2000@aliyun.com>
2022-03-28 08:30:20 +0800

    add

---
+++ springboot-jedis/src/main/java/com/example/springboot/jedis/PublisherAndSubscribe/Publisher.java
@@ -0,0 +1,23 @@
+package com.example.springboot.jedis.PublisherAndSubscribe;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * 消息发布者
+ *
+ * @author Harry Zhang
+ * @since 2020/4/10 01:26
+ */
+public class Publisher {
+    public static final JedisPool POOL = new JedisPool(new JedisPoolConfig(), "localhost");
+
+    public static void main(String[] args) {
+        Jedis jedis = POOL.getResource();
+        jedis.publish("codehole", "python comes");
+        jedis.publish("codehole", "java comes");
+        jedis.publish("codehole", "golang comes");
+    }
+
+}
