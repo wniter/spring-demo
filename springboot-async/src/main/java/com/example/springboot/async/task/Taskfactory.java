d936a81a36bb15cccd0ad3b21a552ee84929c288
Boolean <lyg2000@aliyun.com>
2022-03-29 19:10:06 +0800

    add

--- springboot-async/src/main/java/com/example/springboot/async/task/Taskfactory.java
+++ springboot-async/src/main/java/com/example/springboot/async/task/Taskfactory.java
@@ -36,6 +36,58 @@ public class Taskfactory {
 
 
     }
+    /**
+     * 模拟5秒的异步任务
+     */
+    @Async
+    public Future<Boolean> asyncTask1() throws InterruptedException {
+        doTask("asyncTask1", 5);
+        return new AsyncResult<>(Boolean.TRUE);
+    }
 
+    /**
+     * 模拟2秒的异步任务
+     */
+    @Async
+    public Future<Boolean> asyncTask2() throws InterruptedException {
+        doTask("asyncTask2", 2);
+        return new AsyncResult<>(Boolean.TRUE);
+    }
+
+    /**
+     * 模拟3秒的异步任务
+     */
+    @Async
+    public Future<Boolean> asyncTask3() throws InterruptedException {
+        doTask("asyncTask3", 3);
+        return new AsyncResult<>(Boolean.TRUE);
+    }
+
+    /**
+     * 模拟5秒的同步任务
+     */
+    public void task1() throws InterruptedException {
+        doTask("task1", 5);
+    }
+
+    /**
+     * 模拟2秒的同步任务
+     */
+    public void task2() throws InterruptedException {
+        doTask("task2", 2);
+    }
+
+    /**
+     * 模拟3秒的同步任务
+     */
+    public void task3() throws InterruptedException {
+        doTask("task3", 3);
+    }
+
+    private void doTask1(String taskName, Integer time) throws InterruptedException {
+        log.info("{}开始执行，当前线程名称【{}】", taskName, Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(time);
+        log.info("{}执行成功，当前线程名称【{}】", taskName, Thread.currentThread().getName());
+    }
 
 }
