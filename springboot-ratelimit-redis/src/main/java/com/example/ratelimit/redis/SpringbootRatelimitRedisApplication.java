ffc50b92b95184e9c710ccd6c45ccaa44c61ea37
Boolean <lyg2000@aliyun.com>
2022-03-30 00:24:37 +0800

    add

---
+++ springboot-ratelimit-redis/src/main/java/com/example/ratelimit/redis/SpringbootRatelimitRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratelimit.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRatelimitRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRatelimitRedisApplication.class, args);
+    }
+
+}
