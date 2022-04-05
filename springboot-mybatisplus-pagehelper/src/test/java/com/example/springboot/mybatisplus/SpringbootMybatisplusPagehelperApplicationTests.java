41d0027936244c31b2b0e6b56e310f56e5288e68
Boolean <lyg2000@aliyun.com>
2022-04-06 00:58:58 +0800

    add

--- springboot-mybatisplus-pagehelper/src/test/java/com/example/springboot/mybatisplus/SpringbootMybatisplusPagehelperApplicationTests.java
+++ springboot-mybatisplus-pagehelper/src/test/java/com/example/springboot/mybatisplus/SpringbootMybatisplusPagehelperApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.springboot.mybatisplus;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootMybatisplusPagehelperApplicationTests {
+
+
 
     @Test
     void contextLoads() {
