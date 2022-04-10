7bc2b10bce34a113dcdfdfe2c507a5fb1fbac2fa
Boolean <lyg2000@aliyun.com>
2022-04-11 00:10:13 +0800

    add

--- springboot-scheduled-task/src/main/java/com/example/demo/SpringbootScheduledTaskApplication.java
+++ springboot-scheduled-task/src/main/java/com/example/demo/SpringbootScheduledTaskApplication.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 public class SpringbootScheduledTaskApplication {
