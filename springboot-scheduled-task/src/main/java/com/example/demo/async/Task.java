7e638c9a78e313e3d60fa4adb8355e318a1fa5a8
Boolean <lyg2000@aliyun.com>
2022-04-11 00:15:43 +0800

    add

--- springboot-scheduled-task/src/main/java/com/example/demo/async/Task.java
+++ springboot-scheduled-task/src/main/java/com/example/demo/async/Task.java
@@ -5,7 +5,9 @@ import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author 程序猿DD
@@ -133,4 +135,21 @@ public class Task {
 //        return new AsyncResult<>("test");
 //    }
 //
+//}
+//
+//@EnableAsync
+//@Configuration
+//class TaskPoolConfig {
+//
+//    @Bean("taskExecutor")
+//    public Executor taskExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(10);
+//        executor.setMaxPoolSize(20);
+//        executor.setQueueCapacity(200);
+//        executor.setKeepAliveSeconds(60);
+//        executor.setThreadNamePrefix("taskExecutor-");
+//        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+//        return executor;
+//    }
 //}
