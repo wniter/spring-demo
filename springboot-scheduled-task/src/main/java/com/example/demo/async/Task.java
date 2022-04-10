e50324e40da3c07efaf3472c4c23f3cd5dfc3f9f
Boolean <lyg2000@aliyun.com>
2022-04-11 00:11:38 +0800

    add

--- springboot-scheduled-task/src/main/java/com/example/demo/async/Task.java
+++ springboot-scheduled-task/src/main/java/com/example/demo/async/Task.java
@@ -118,3 +118,19 @@ public class Task {
 //    }
 //
 //}
+//@Slf4j
+//@Component
+//public class Task {
+//
+//    public static Random random = new Random();
+//
+//    @Async("taskExecutor")
+//    public Future<String> run() throws Exception {
+//        long sleep = random.nextInt(10000);
+//        log.info("开始任务，需耗时：" + sleep + "毫秒");
+//        Thread.sleep(sleep);
+//        log.info("完成任务");
+//        return new AsyncResult<>("test");
+//    }
+//
+//}
